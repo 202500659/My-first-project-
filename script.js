e63a1e5196a9617352e86bbd08ab079443b974a7
202500659 <202500659@sigc.edu>
2026-09-23 21:34:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,4 +10,9 @@ function homeAI(){
   "<br>Amazon" +
   "<br>Flipkart" +
   "<br>IKEA";
+function showLoading(id, callback){
+  const box = document.getElementById(id);
+  box.innerHTML = "🤖 Analyzing your budget...";
+  setTimeout(callback, 1200);
+}
 }
